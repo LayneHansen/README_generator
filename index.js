f648069426d2b5a55cd98d3404460d6bea3321b8
LayneHansen <layne.d.hansen@gmail.com>
2020-09-18 20:17:53 -0600

    added index.js from homework file, ran npm init, installed inquirer, wrote prompt questions

---
+++ index.js
@@ -0,0 +1,60 @@
+// array of questions for user
+var inquirer = require("inquirer");
+console.log(inquirer);
+
+inquirer
+.prompt([
+{
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "What is the description of your project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "What are the installation instructions for your project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "What is the usage information for your project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "Who contributed to your project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "Are there any tests included in your project?",
+    name: "title"
+},
+
+
+])
+
+.then(function(response) {
+ console.log(reponse);
+ if (response) {
+ const {title, description, installation, usage, contributor, tests}
+ console.log(title, description, installation, usage, contributor, tests);
+ }
+
+})
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
